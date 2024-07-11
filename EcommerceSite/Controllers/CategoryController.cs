146ab9e9a3a1ed14ed09e15b214a0b3bd7f4e687
Mizan <mizanurcse12@gmail.com>
2024-07-11 13:34:43 +0600

    Making Forms Using Bootstrap

--- EcommerceSite/Controllers/CategoryController.cs
+++ EcommerceSite/Controllers/CategoryController.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcommerceSite.Data;
+using EcommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcommerceSite.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {   
+            List<Category> objectCategoryList = _db.Categories.ToList();
+            return View(objectCategoryList);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
