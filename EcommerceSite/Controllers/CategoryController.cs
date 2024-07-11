0be115dafb29ba02814b2acd3cfe25056f2d37c4
Mizan <mizanurcse12@gmail.com>
2024-07-11 16:07:18 +0600

    Form Server and Client Side Validation

--- EcommerceSite/Controllers/CategoryController.cs
+++ EcommerceSite/Controllers/CategoryController.cs
@@ -21,5 +21,21 @@ namespace EcommerceSite.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "Name and Display Order can not be the same");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("index", "category");
+            }
+            return View();
+            
+        }
     }
 }
